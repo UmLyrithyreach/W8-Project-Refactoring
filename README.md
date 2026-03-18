548292008badbfd961a978c03b7211ad686da040
Um Lyrithyreach <umlyrithyreach@gmail.com>
2026-03-18 21:48:05 +0700

    First Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# week8_before_practice
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/week8_before_practice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.week8_before_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
