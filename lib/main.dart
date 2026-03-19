325e739d60e2982aa47d913907919759c786110a
Um Lyrithyreach <umlyrithyreach@gmail.com>
2026-03-19 14:31:20 +0700

    Remove Unused main

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
-import 'main_dev.dart';
+import 'package:flutter/material.dart';
+import 'main_common.dart';
+import 'repositories/mock_location_repository.dart';
+import 'repositories/mock_rides_repository.dart';
+import 'repositories/mock_ride_preference_repository.dart';
+
+void main() {
+  final app = createApp(
+    locationRepository: MockLocationRepository(),
+    ridesRepository: MockRidesRepository(),
+    ridePreferenceRepository: MockRidePreferenceRepository(),
+  );
+  runApp(app);
+}
